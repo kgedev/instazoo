411493eefba0c00a24f827e1ba0a7d0d1c3f55ad
glebus <svarog96@gmail.com>
2021-05-05 20:01:29 +0500

    init app

---
+++ src/main/kotlin/com/example/instazoo/InstazooApplication.kt
@@ -0,0 +1,11 @@
+package com.example.instazoo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InstazooApplication
+
+fun main(args: Array<String>) {
+	runApplication<InstazooApplication>(*args)
+}
